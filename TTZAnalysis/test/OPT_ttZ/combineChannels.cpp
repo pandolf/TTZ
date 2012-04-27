488e16d640b64add9022d5723705b55dfa5fe033
pandolf <>
2012-04-27 10:50:09 +0000

    added computeZPL for multiple channels, not implemented yet

--- TTZAnalysis/test/OPT_ttZ/combineChannels.cpp
+++ TTZAnalysis/test/OPT_ttZ/combineChannels.cpp
@@ -20,26 +20,52 @@ int main() {
   float obs_TTZTL, b_TTZTL, b_err_TTZTL;
   ifs_TTZTL >> obs_TTZTL >> b_TTZTL >> b_err_TTZTL;
 
-  float obs_SSDL, b_SSDL, b_err_SSDL;
-  ifs_SSDL >> obs_SSDL >> b_SSDL >> b_err_SSDL;
+  float obs_SSDL_ee, b_SSDL_ee, b_err_SSDL_ee;
+  float obs_SSDL_em, b_SSDL_em, b_err_SSDL_em;
+  float obs_SSDL_mm, b_SSDL_mm, b_err_SSDL_mm;
+  float obs_SSDL_tot, b_SSDL_tot, b_err_SSDL_tot;
+  std::string name;
+  ifs_SSDL >> name >> obs_SSDL_mm >> b_SSDL_mm >> b_err_SSDL_mm;
+  ifs_SSDL >> name >> obs_SSDL_em >> b_SSDL_em >> b_err_SSDL_em;
+  ifs_SSDL >> name >> obs_SSDL_ee >> b_SSDL_ee >> b_err_SSDL_ee;
+  ifs_SSDL >> name >> obs_SSDL_tot >> b_SSDL_tot >> b_err_SSDL_tot;
 
+  StatChannel channel_SSDL_mm("mm", obs_SSDL_mm, b_SSDL_mm, b_err_SSDL_mm);
+  StatChannel channel_SSDL_em("em", obs_SSDL_em, b_SSDL_em, b_err_SSDL_em);
+  StatChannel channel_SSDL_ee("ee", obs_SSDL_ee, b_SSDL_ee, b_err_SSDL_ee);
 
-  float ZBi_SSDL  = StatTools::computeZBi( obs_SSDL, b_SSDL, b_err_SSDL );
+  std::vector< StatChannel > channels_SSDL;
+  channels_SSDL.push_back(channel_SSDL_mm);
+  channels_SSDL.push_back(channel_SSDL_em);
+  channels_SSDL.push_back(channel_SSDL_ee);
+  
+
+  float ZBi_SSDL  = StatTools::computeZBi( obs_SSDL_tot, b_SSDL_tot, b_err_SSDL_tot );
   float ZBi_TTZTL = StatTools::computeZBi( obs_TTZTL, b_TTZTL, b_err_TTZTL );
 
-  float ZPL_SSDL  = StatTools::computeZPL( obs_SSDL, b_SSDL, b_err_SSDL );
+  float ZPL_SSDL_ee  = StatTools::computeZPL( obs_SSDL_ee, b_SSDL_ee, b_err_SSDL_ee );
+  float ZPL_SSDL_em  = StatTools::computeZPL( obs_SSDL_em, b_SSDL_em, b_err_SSDL_em );
+  float ZPL_SSDL_mm  = StatTools::computeZPL( obs_SSDL_mm, b_SSDL_mm, b_err_SSDL_mm );
+  float ZPL_SSDL_tot  = StatTools::computeZPL( obs_SSDL_tot, b_SSDL_tot, b_err_SSDL_tot );
+  float ZPL_SSDL_comb  = StatTools::computeZPL( channels_SSDL );
+
+  std::cout <<  "ZPL_SSDL_ee :" <<  ZPL_SSDL_ee << std::endl;
+  std::cout <<  "ZPL_SSDL_em :" <<  ZPL_SSDL_em << std::endl;
+  std::cout <<  "ZPL_SSDL_mm :" <<  ZPL_SSDL_mm << std::endl;
+  std::cout <<  "ZPL_SSDL_tot:" <<  ZPL_SSDL_tot<< std::endl;
+
   float ZPL_TTZTL = StatTools::computeZPL( obs_TTZTL, b_TTZTL, b_err_TTZTL );
 
-  TF2* f2_lik_SSDL = StatTools::getLikelihoodFunction( "lik_SSDL", obs_SSDL, b_SSDL, b_err_SSDL );
+  TF2* f2_lik_SSDL = StatTools::getLikelihoodFunction( "lik_SSDL", obs_SSDL_tot, b_SSDL_tot, b_err_SSDL_tot );
   TF2* f2_lik_TTZTL = StatTools::getLikelihoodFunction( "lik_TTZTL", obs_TTZTL, b_TTZTL, b_err_TTZTL );
 
-  TF2* f2_lik_combined = new TF2("lik_combined", "lik_SSDL*lik_TTZTL", 0., obs_SSDL+obs_TTZTL-b_SSDL-b_TTZTL+5., 0., 2.*(b_TTZTL+b_SSDL) );
+  TF2* f2_lik_combined = new TF2("lik_combined", "lik_SSDL*lik_TTZTL", 0., obs_SSDL_tot+obs_TTZTL-b_SSDL_tot-b_TTZTL+5., 0., 2.*(b_TTZTL+b_SSDL_tot) );
 
 
   std::cout << std::endl << std::endl;
   std::cout << "SSDL Channel: " << std::endl;
-  std::cout << "Observed: " << obs_SSDL << "         Expected Background: " << b_SSDL << " +- " << b_err_SSDL << std::endl;
-  std::cout << "Z_PL (SSDL): " << ZPL_SSDL << "   ( ZBi: " << ZBi_SSDL << " )" << std::endl;
+  std::cout << "Observed: " << obs_SSDL_tot << "         Expected Background: " << b_SSDL_tot << " +- " << b_err_SSDL_tot << std::endl;
+  std::cout << "Z_PL (SSDL): " << ZPL_SSDL_tot << "   ( ZBi: " << ZBi_SSDL << " )" << std::endl;
   
   std::cout << std::endl << std::endl;
   std::cout << "TTZTL Channel: " << std::endl;
