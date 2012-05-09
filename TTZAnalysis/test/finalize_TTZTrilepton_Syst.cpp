e9cba39318afd9dbc7f61586d0336c2429fba53b
pandolf <>
2012-05-09 08:12:13 +0000

    added one run with no syst

--- TTZAnalysis/test/finalize_TTZTrilepton_Syst.cpp
+++ TTZAnalysis/test/finalize_TTZTrilepton_Syst.cpp
@@ -39,6 +39,10 @@ int main( int argc, char* argv[] ) {
   Ntp1Finalizer_TTZTrilepton* nf = new Ntp1Finalizer_TTZTrilepton( "syst", selectionType, bTaggerType, leptType );
   nf->set_inputAnalyzerType("TTZ");
 
+  // run once with no syst
+  runOnAllBackgrounds( nf );
+
+
   std::cout << std::endl << "++++++++++++++++++++++" << std::endl;
   std::cout << "+++ BTag Syst +1 Sigma" << std::endl;
   std::cout << "++++++++++++++++++++++" << std::endl << std::endl;
@@ -81,21 +85,41 @@ int main( int argc, char* argv[] ) {
 void runOnAllBackgrounds( Ntp1Finalizer_TTZTrilepton* nf ) {
 
   
-  runOnSingleBackground( nf, "TTJ_Fall11_highstat" );
-  runOnSingleBackground( nf, "DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola_Fall11" );
-  runOnSingleBackground( nf, "VV_Summer11" );
+  runOnSingleBackground( nf, "BG" );
+  //runOnSingleBackground( nf, "VV_Summer11" );
+  //runOnSingleBackground( nf, "TTJ_Fall11_highstat" );
+  //runOnSingleBackground( nf, "DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola_Fall11" );
 
 }
 
 
 void runOnSingleBackground( Ntp1Finalizer_TTZTrilepton* nf, const std::string& dataset ) {
 
-  Ntp1Finalizer_TTZTrilepton* nf_syst = new Ntp1Finalizer_TTZTrilepton( *nf );
+  nf->clearTree();
 
-  nf_syst->addFile( dataset );
+  Ntp1Finalizer_TTZTrilepton* nf_syst = new Ntp1Finalizer_TTZTrilepton( *nf );
+  nf_syst->set_dataset(dataset);
+
+  if( dataset=="VV_Summer11" ) {
+  
+    nf_syst->addFile("WZJetsTo3LNu_TuneZ2_7TeV-madgraph-tauola_Summer11-PU_S4_START42_V11-v1");
+    nf_syst->addFile("ZZ_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1");
+    nf_syst->addFile("WW_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1");
+  
+  } else if( dataset=="BG" ) {
+  
+    nf_syst->addFile("WZJetsTo3LNu_TuneZ2_7TeV-madgraph-tauola_Summer11-PU_S4_START42_V11-v1");
+    nf_syst->addFile("ZZ_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1");
+    nf_syst->addFile("WW_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1");
+    nf_syst->addFile("TTJ_Fall11_highstat");
+    nf_syst->addFile("DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola_Fall11");
+  
+  } else {
+
+    nf_syst->addFile( dataset );
+ 
+  }
 
   nf_syst->finalize();
-
-  delete nf;
 
 } 
