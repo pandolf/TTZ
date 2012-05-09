2a0ed11597270d1417d265c3f229b14f3d5715d3
pandolf <>
2012-05-09 09:27:11 +0000

    first commit

---
+++ TTZAnalysis/test/combineComand.sh
@@ -0,0 +1,3 @@
+combine -M ProfileLikelihood --signif TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/datacard.txt
+
+#combine -M HybridNew --testStat LHC --signif -i 3 -T 1000 --saveHybridResult TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/datacard.txt
