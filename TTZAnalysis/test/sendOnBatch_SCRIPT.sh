c3952321df378684594093d2b1c9bc31543ecd4c
pandolf <>
2012-05-30 14:45:16 +0000

    first commit

---
+++ TTZAnalysis/test/sendOnBatch_SCRIPT.sh
@@ -0,0 +1,28 @@
+# usage: source sendOnBatch_SCRIPT.sh [analyzerType="TTZ"]
+
+# python sendOnBatch.py TTZ_TuneZ2_7TeV-madgraphCMSSW42xPUv3_spadhi 2 $1
+# python sendOnBatch.py TTW_TuneZ2_7TeV-madgraphCMSSW42xPUv2_spadhi 2 $1
+# python sendOnBatch.py TTPhoton_TuneZ2_7TeV-madgraphCMSSW42xPUv3 2 $1
+# python sendOnBatch.py WZJetsTo3LNu_TuneZ2_7TeV-madgraph-tauola_Summer11-PU_S4_START42_V11-v1 2 $1
+# python sendOnBatch.py ZZ_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1 5 $1
+# python sendOnBatch.py WW_TuneZ2_7TeV_pythia6_tauola_Summer11-PU_S4_START42_V11-v1 5 $1
+# python sendOnBatch.py TTJ_Fall11_highstat 10 $1
+#echo "sleeping"
+#sleep 3000
+# python sendOnBatch.py DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola_Fall11 4 $1
+#echo "sleeping"
+#sleep 3001
+# python sendOnBatch.py DoubleMu_Run2011A_FULL 10 $1
+# python sendOnBatch.py DoubleMu_Run2011B_v2 10 $1
+# python sendOnBatch.py DoubleElectron_Run2011A_FULL 10 $1
+# python sendOnBatch.py DoubleElectron_Run2011B_v2 10 $1
+# python sendOnBatch.py MuEG_Run2011A_FULL 10 $1
+# python sendOnBatch.py MuEG_Run2011B 10 $1
+# python sendOnBatch.py SingleMu_Run2011A_FULL 10 $1
+# python sendOnBatch.py SingleMu_Run2011B_v2 10 $1
+#echo "sleeping"
+#sleep 3000
+# python sendOnBatch.py WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_Summer11 10 $1
+
+#python sendOnBatch.py DYToEE_M-20_CT10_TuneZ2_7TeV-powheg-pythia 5 $1
+#python sendOnBatch.py DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia 5 $1
