34fbda037017dff3d8742d86f30d0333e25de82b
pandolf <>
2012-05-31 11:43:25 +0000

    now correctly copying the pdf not the eps of the measurement fig

--- TTZAnalysis/test/copyToPAS.sh
+++ TTZAnalysis/test/copyToPAS.sh
@@ -16,3 +16,5 @@ epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel_s
 mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel_scaled.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/TOP-12-014/trunk/figures/trilepton/bg_rescaling/
 epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields.eps
 mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/TOP-12-014/trunk/figures/trilepton/results
+epstopdf measurement_vs_NLO.eps
+mv measurement_vs_NLO.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/TOP-12-014/trunk/figures/
