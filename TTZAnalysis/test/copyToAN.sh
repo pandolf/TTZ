380ee8a542045d975595d1ebf2707c5e22cadbaa
pandolf <>
2012-05-31 10:27:57 +0000

    first commit

---
+++ TTZAnalysis/test/copyToAN.sh
@@ -0,0 +1,54 @@
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ht.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ht.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ptLeptZ1.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ptLeptZ1.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/pfMet.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/pfMet.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ptZll.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/ptZll.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/nBJets_loose.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/nBJets_loose.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/nBJets_medium.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/nBJets_medium.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/kinematics/data/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/channelYields_noScaling.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/channelYields_noScaling.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/channelYields.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_plusZ_TCHE_ALL/channelYields.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/nvertex_PUW.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/nvertex_PUW.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/rhoPF_prepresel.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/rhoPF_prepresel.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_prepresel.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_prepresel.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_noscaling.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_noscaling.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_antibtag.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_antibtag.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_scaled.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_presel_scaled.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/ttbarChiSquareScan.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/ttbarChiSquareScan.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel_scaled.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_presel_TCHE_ALL/mZll_OF_prepresel_scaled.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/bg_rescaling/
+
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/results
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nJets_SignalOnlyJERSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nJets_SignalOnlyJERSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nJets_SignalOnlyJESSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nJets_SignalOnlyJESSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyJERSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyJERSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyJESSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyJESSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nBJets_loose_SignalOnlyBTagSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nBJets_loose_SignalOnlyBTagSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nBJets_medium_SignalOnlyBTagSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/nBJets_medium_SignalOnlyBTagSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyBTagSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyBTagSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
+epstopdf TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyLeptSyst.eps
+mv TTZTrileptonPlots_DATA_Run2011_FULL_optsel3_TCHE_ALL/channelYields_SignalOnlyLeptSyst.pdf /afs/cern.ch/user/p/pandolf/tdr2/notes/AN-12-184/trunk/figures/trilepton/syst
