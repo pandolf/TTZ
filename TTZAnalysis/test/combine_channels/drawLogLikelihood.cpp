f1d88a23f92d21d076ecfd30de0a9ef01e3a812f
pandolf <>
2012-06-14 09:49:39 +0000

    first commit

---
+++ TTZAnalysis/test/combine_channels/drawLogLikelihood.cpp
@@ -0,0 +1,77 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TArrow.h"
+
+#include "DrawBase.h"
+
+
+
+
+int main() {
+
+
+  std::string suffix = "0_120613_091632"; 
+
+  DrawBase* db = new DrawBase(suffix);
+  db->set_lumi(4980.); 
+
+  std::string fileName = "q_mergedToys_crab_" + suffix + ".root";
+
+  TFile* qFile = TFile::Open(fileName.c_str());
+
+  TTree* qTree = (TTree*)qFile->Get("q");
+
+  float xMax = 30.;
+
+  TH1D* h1_bg = new TH1D("bg", "", 50, 0., xMax);
+  TH1D* h1_obs = new TH1D("obs", "", 50, 0., xMax);
+
+  qTree->Project("bg", "2.*q", "type==-1");
+  qTree->Project("obs", "2.*q", "type==0");
+
+  h1_bg->SetFillStyle(3004);
+  h1_bg->SetFillColor(39);
+  h1_bg->SetLineColor(39);
+  h1_bg->SetLineWidth(2.);
+
+  float yMin = 0.5;
+  TH2D* h2_axes = new TH2D("axes", "", 10, 0., xMax, 10, yMin, 5000000.);
+  h2_axes->SetXTitle("2 ln(L)");
+  h2_axes->SetYTitle("Number of Toys");
+
+  TPaveText* labelSqrt = db->get_labelSqrt();
+
+  float xArrow = h1_obs->GetMean();
+
+  TArrow* arrow_obs = new TArrow( xArrow, 500., xArrow, yMin, 0.05, ">" );
+  arrow_obs->SetLineColor(46);
+  arrow_obs->SetFillColor(46);
+  arrow_obs->SetLineWidth(2.);
+
+  TLegend* legend = new TLegend( 0.4, 0.7, 0.88, 0.88);
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.038);
+  legend->AddEntry( h1_bg, "Background Hypothesis", "F" );
+  legend->AddEntry( arrow_obs, "Observed", "L" );
+
+  TCanvas* c1 = new TCanvas( "c1", "", 600, 600);
+  c1->cd();
+  c1->SetLogy();
+
+  h2_axes->Draw();
+  h1_bg->Draw("same");
+  arrow_obs->Draw();
+  labelSqrt->Draw("same");
+  legend->Draw("Same");
+
+  gPad->RedrawAxis();
+  c1->SaveAs("prova.eps");
+
+  return 0;
+
+}
+
