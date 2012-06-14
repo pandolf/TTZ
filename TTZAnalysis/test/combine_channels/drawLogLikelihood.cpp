c5937c62039c2aaf224615a5adf84dd14004a115
pandolf <>
2012-06-14 12:37:55 +0000

    now doing all three plots

--- TTZAnalysis/test/combine_channels/drawLogLikelihood.cpp
+++ TTZAnalysis/test/combine_channels/drawLogLikelihood.cpp
@@ -9,17 +9,28 @@
 #include "DrawBase.h"
 
 
+void drawSingleLikelihoodPlot( const std::string& suffix );
 
 
 int main() {
 
 
-  std::string suffix = "0_120613_091632"; 
+  drawSingleLikelihoodPlot( "trilepton_4channels" );
+  drawSingleLikelihoodPlot( "ssdl_3channels" );
+  drawSingleLikelihoodPlot( "7channels" );
+
+  return 0;
+
+}
+
+
+
+void drawSingleLikelihoodPlot( const std::string& suffix ) {
 
   DrawBase* db = new DrawBase(suffix);
   db->set_lumi(4980.); 
 
-  std::string fileName = "q_mergedToys_crab_" + suffix + ".root";
+  std::string fileName = "q_mergedToys_" + suffix + ".root";
 
   TFile* qFile = TFile::Open(fileName.c_str());
 
@@ -39,7 +50,7 @@ int main() {
   h1_bg->SetLineWidth(2.);
 
   float yMin = 0.5;
-  TH2D* h2_axes = new TH2D("axes", "", 10, 0., xMax, 10, yMin, 5000000.);
+  TH2D* h2_axes = new TH2D("axes", "", 10, 0., xMax, 10, yMin, 10.*h1_bg->GetMaximum());
   h2_axes->SetXTitle("2 ln(L)");
   h2_axes->SetYTitle("Number of Toys");
 
@@ -52,7 +63,12 @@ int main() {
   arrow_obs->SetFillColor(46);
   arrow_obs->SetLineWidth(2.);
 
-  TLegend* legend = new TLegend( 0.4, 0.7, 0.88, 0.88);
+  std::string legendTitle;
+  if( suffix=="trilepton_4channels" ) legendTitle = "Trilepton Channel";
+  else if( suffix=="ssdl_3channels" ) legendTitle = "Dilepton Channel";
+  else                                legendTitle = "7 Channels Combined";
+
+  TLegend* legend = new TLegend( 0.4, 0.7, 0.72, 0.9, legendTitle.c_str());
   legend->SetFillColor(0);
   legend->SetTextSize(0.038);
   legend->AddEntry( h1_bg, "Background Hypothesis", "F" );
@@ -68,10 +84,19 @@ int main() {
   labelSqrt->Draw("same");
   legend->Draw("Same");
 
-  gPad->RedrawAxis();
-  c1->SaveAs("prova.eps");
 
-  return 0;
+  gPad->RedrawAxis();
+
+  std::string canvasName = "logLikelihood_" + suffix + ".eps";
+
+  c1->SaveAs(canvasName.c_str());
+
+  delete c1;
+  delete h2_axes;
+  delete legend;
+  delete h1_bg;
+  delete h1_obs;
+  delete arrow_obs;
 
 }
 
