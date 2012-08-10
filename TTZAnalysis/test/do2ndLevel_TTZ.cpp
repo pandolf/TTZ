5b0784443df17bfd11273af178d947a74004b0ca
pandolf <>
2012-08-10 08:25:17 +0000

    added trigger paths

--- TTZAnalysis/test/do2ndLevel_TTZ.cpp
+++ TTZAnalysis/test/do2ndLevel_TTZ.cpp
@@ -1,6 +1,8 @@
 #include "Ntp1Analyzer_TTZ.h"
 #include <stdlib.h>
 
+#include "TString.h"
+
 
 
 int main( int argc, char* argv[]) {
@@ -21,10 +23,36 @@ int main( int argc, char* argv[]) {
     na = new Ntp1Analyzer_TTZ(dataset, flags);
   }
 
+  TString dataset_tstr(dataset);
 
- // na->AddRequiredTrigger( "HLT_DoubleMu7" );
- // na->AddRequiredTrigger( "HLT_Mu13_Mu8" );
- // na->AddRequiredTrigger( "HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL" );
+  if( dataset_tstr.Contains("Run2011") || dataset_tstr.Contains("Run2012") ) { //is data
+
+    if( dataset_tstr.BeginsWith("DoubleMu") ) {
+
+      na->AddRequiredTrigger( "HLT_DoubleMu7_v" );
+      na->AddRequiredTrigger( "HLT_Mu13_Mu8_v" );
+      na->AddRequiredTrigger( "HLT_Mu17_Mu8_v" );
+
+    } else if( dataset_tstr.BeginsWith("DoubleElectron") ) {
+
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v" );
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v" );
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v" );
+ 
+    } else if( dataset_tstr.BeginsWith("MuEG") ) {
+
+      na->AddRequiredTrigger( "HLT_Mu17_Ele8_CaloIdL_v" );
+      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdL_v" );
+      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v" );
+
+    } else if( dataset_tstr.BeginsWith("SingleMu") ) {
+
+      na->AddRequiredTrigger( "HLT_IsoMu24_v" );
+
+    }
+
+  }  //if is data
+
 
 
   if( argc==2 ) {
