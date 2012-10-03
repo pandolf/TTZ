38302deb7aec6eeb7fd2358b152f27328bd9114c
pandolf <>
2012-10-03 11:18:03 +0000

    works

--- TTZAnalysis/test/do2ndLevel_LeptonStudies.cpp
+++ TTZAnalysis/test/do2ndLevel_LeptonStudies.cpp
@@ -25,43 +25,6 @@ int main( int argc, char* argv[]) {
 
   TString dataset_tstr(dataset);
 
-  bool isData2011 = dataset_tstr.Contains("Run2011");
-  bool isData2012 = dataset_tstr.Contains("Run2012");
-
-  if( isData2011 || isData2012 ) {
-
-    if( dataset_tstr.BeginsWith("DoubleMu") ) {
-
-      na->AddRequiredTrigger( "HLT_DoubleMu7_v" );
-      na->AddRequiredTrigger( "HLT_Mu13_Mu8_v" );
-      na->AddRequiredTrigger( "HLT_Mu17_Mu8_v" );
-
-    } else if( dataset_tstr.BeginsWith("DoubleElectron") ) {
-
-      na->AddRequiredTrigger( "HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v" );
-      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v" );
-      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v" );
- 
-    } else if( dataset_tstr.BeginsWith("MuEG") ) {
-
-      na->AddRequiredTrigger( "HLT_Mu17_Ele8_CaloIdL_v" );
-      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdL_v" );
-      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v" );
-
-    } else if( dataset_tstr.BeginsWith("SingleMu") ) {
-
-      na->AddRequiredTrigger( "HLT_IsoMu24_v" );
-
-    }
-
-    if( isData2011 )
-      na->ReadJSONFile("Cert_160404-180252_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
-    if( isData2012 )
-      na->ReadJSONFile("Cert_190456-200245_8TeV_PromptReco_Collisions12_CMSSWConfig.txt");
-
-  }  //if is data
-
-
 
   if( argc==2 ) {
     std::string fileName = "files_" + dataset + ".txt";
